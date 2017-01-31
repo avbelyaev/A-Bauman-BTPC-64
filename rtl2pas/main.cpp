c72a3a736e053d376d21063add3a6e695edf2d33
avbelyaev <antonsdl@gmail.com>
2017-02-01 01:06:22 +0300

    init

---
+++ rtl2pas/main.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+#define PASCAL_STR_LEN_MAX 255
+
+typedef uint8_t BYTE;
+
+string NOP = "$90";
+
+bool isExecutable(FILE *f) {
+
+    if ('M' == fgetc(f) && 'Z' == fgetc(f))
+        return true;
+
+    rewind(f);
+    fgetc(f);
+    return  'E' == fgetc(f) &&
+            'L' == fgetc(f) &&
+            'F' == fgetc(f);
+}
+
+int main() {
+
+    FILE *f = fopen(
+            "/home/anthony/Dropbox/bero/beronew",
+            //"/home/anthony/Dropbox/bero/x32/bero32",
+            //"/home/anthony/Dropbox/bero/btpc.exe",
+            "rb");
+
+    if (isExecutable(f)) {
+        rewind(f);
+
+        vector<string> stringList;
+        stringList.clear();
+
+        stringList.push_back("  OutputCodeDataSize:=0;");
+
+        string s = "  OutputCodeString(";
+        int singleByte;
+
+        int bytesInString = 0;
+        int bytes = 0;
+        while (!feof(f)) {
+
+            if (PASCAL_STR_LEN_MAX == bytesInString) {
+                s += ");";
+                stringList.push_back(s);
+
+                bytesInString = 0;
+                s = "  OutputCodeString(";
+            }
+
+            singleByte = fgetc(f);
+            s += "#" + to_string(singleByte);
+
+            bytesInString++;
+            bytes++;
+        }
+
+        while (bytesInString < PASCAL_STR_LEN_MAX) {
+
+            s += "#" + NOP;
+            bytesInString++;
+        }
+        s += ");";
+
+        stringList.push_back(s);
+        stringList.push_back("  OutputCodeDataSize:=" + to_string(bytes) + ";");
+
+        for (int i = 0; i < bytes / PASCAL_STR_LEN_MAX + 1 + 2; i++) {
+
+            cout << stringList[i] << endl;
+        }
+
+    } else {
+        cout << "file is not of ELF format" << endl;
+    }
+
+    return 0;
+}
