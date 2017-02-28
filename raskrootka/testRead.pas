5b19c62e7e49ff158ebf740c5c559e566b2e2e8f
avbelyaev <antonsdl@gmail.com>
2017-02-28 19:05:28 +0300

    KAV improvements. Read() seems to work

--- raskrootka/testRead.pas
+++ raskrootka/testRead.pas
@@ -1,6 +1,23 @@
 program RTLWriteTest;
-var a:integer;
+
+type TSignature = array[1..5] of integer;
+
+var a,i:integer;
+    m:TSignature;
+
 begin
-  Read(a);
-  WriteLn('[', a, ']');
+
+  i:=1;
+  while i <= 3 do begin
+    Read(a);
+    WriteLn('->', a);
+    m[i]:=a;
+    i:=i+1;
+  end;
+
+  i:=1;
+  while i <= 3 do begin
+    WriteLn(m[i]);
+    i:=i+1;
+  end;
 end.
